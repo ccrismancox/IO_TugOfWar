f6a90d1ae03f4c44d2c4ff8e3bf0932cf0643bc5
ccrismancox <ccrismancox@gmail.com>
2024-09-24 11:16:55 -0500

    testing 4.1

---
+++ Installation/Rpackages_4.1_testing.R
@@ -0,0 +1,51 @@
+.libPaths(unique(c("~/R/x86_64-pc-linux-gnu-library/4.1",.libPaths())))
+install.packages("remotes")
+library(remotes)
+
+install_version("ggplot2", "3.4.3")
+install_version("stringr", "1.4.0")
+install_version("data.table", "1.14.0")
+install_version("bit64", "4.0.5")
+install_version("bit", "4.0.4")
+install_version("MARSS", "3.11.3")
+install_version("knitr", "1.44")
+install_version("stargazer", "5.2.3")
+install_version("doParallel", "1.0.16")
+install_version("iterators", "1.0.14")
+install_version("doRNG", "1.8.6")
+install_version("rngtools", "1.5.2")
+install_version("foreach", "1.5.2")
+install_version("car", "3.0-11")
+install_version("moments", "0.14")
+install_version("Matrix", "1.6-5")
+install_version("matrixStats", "0.60.1")
+install_version("rootSolve", "1.8.2.3")
+install_version("ivreg", "0.6-0")
+install_version("cragg", "0.0.1")
+install_version("sensemakr", "0.1.6")
+install_version("rsq", "2.6")
+install_version("metR", "0.15.0")
+install_version("gridExtra", "2.3")
+install_version("tipr", "1.0.2")
+install_version("exactextractr", "0.10.0")
+install_version("raster", "3.6-26")
+install_version("sp", "1.4-5")
+install_version("sf", "1.0-18")
+install_version("ncdf4", "1.23")
+install_version("modelsummary", "0.9.1")
+install_version("vars", "1.5-9")
+install_version("lmtest", "0.9-38")
+install_version("urca", "1.3-0")
+install_version("sandwich", "3.0-2")
+install_version("zoo", "1.8-10")
+install_version("MASS", "7.3-55")
+install_version("numDeriv", "2016.8-1.1")
+
+
+
+
+    
+
+
+
+
